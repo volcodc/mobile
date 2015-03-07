8ef2a19377f15c11bda238ff7326813d7f11cde8
volcodc <volcodc@gmail.com>
2015-03-06 23:27:08 -0600

    second commit

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,10 +5,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	firstObject: function(){
 		var model = this.get('model');
-		return Ember.Object.create({ arr: model.slice(0, 1) })
+		return Ember.Object.create({ arr: model.slice(0, 1) });
 	}.property('model'),
 	otherObjects: function(){
 		var model = this.get('model');
-		return Ember.Object.create({ arr:model.slice(1) })
+		return Ember.Object.create({ arr:model.slice(1) });
 	}.property('model')
 });
